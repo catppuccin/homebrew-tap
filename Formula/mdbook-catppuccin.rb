b835db0522f169aca030b3ed572a0d88351595bc
sgoudham <sgoudham@gmail.com>
2022-08-29 05:06:45 +0100

    build(ci): homepage should be before url

--- Formula/mdbook-catppuccin.rb
+++ Formula/mdbook-catppuccin.rb
@@ -1,8 +1,8 @@
 class MdbookCatppuccin < Formula
   desc "🎊 Soothing pastel theme for mdBook"
+  homepage "https://github.com/catppuccin/mdBook"
   url "https://github.com/catppuccin/mdBook/releases/download/v0.1.1/mdbook-catppuccin-x86_64-apple-darwin.tar.gz"
   sha256 "7a0a2d543fbaa815bdc522491af75b182d0c1be2855c1562a35baa1f9fe23713"
-  homepage "https://github.com/catppuccin/mdBook"
   license "MIT"
 
   def install
