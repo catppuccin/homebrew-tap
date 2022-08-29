382e4ef223d1866e5f4882fc5e3240d88b9f0a53
sgoudham <sgoudham@gmail.com>
2022-08-29 04:49:03 +0100

    build(ci): Try separate urls and sha256s

--- Formula/mdbook-catppuccin.rb
+++ Formula/mdbook-catppuccin.rb
@@ -1,9 +1,17 @@
 class MdbookCatppuccin < Formula
   desc "🎊 Soothing pastel theme for mdBook"
   homepage "https://github.com/catppuccin/mdBook"
-  url "https://github.com/catppuccin/mdBook/releases/download/v0.1.1/mdbook-catppuccin-x86_64-apple-darwin.tar.gz"
-  sha256 "7a0a2d543fbaa815bdc522491af75b182d0c1be2855c1562a35baa1f9fe23713"
   license "MIT"
+
+  on_macos do
+    url "https://github.com/catppuccin/mdBook/releases/download/v0.1.1/mdbook-catppuccin-x86_64-apple-darwin.tar.gz"
+    sha256 "7a0a2d543fbaa815bdc522491af75b182d0c1be2855c1562a35baa1f9fe23713"
+  end
+
+  on_linux do
+    url "https://github.com/catppuccin/mdBook/releases/download/v0.1.1/mdbook-catppuccin-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "9e6cd573de0afdb4838ab71a3d23db4cf29a3d51468cba60579819ed11c15c11"
+  end
 
   def install
     bin.install "mdbook-catppuccin"
