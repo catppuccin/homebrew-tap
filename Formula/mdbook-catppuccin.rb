1a2ed6ba024832e8e80c4c72b961f64335572090
sgoudham <sgoudham@gmail.com>
2022-08-29 04:33:48 +0100

    build(ci): Update sha256

--- Formula/mdbook-catppuccin.rb
+++ Formula/mdbook-catppuccin.rb
@@ -2,7 +2,7 @@ class MdbookCatppuccin < Formula
   desc "🎊 Soothing pastel theme for mdBook"
   homepage "https://github.com/catppuccin/mdBook"
   url "https://github.com/catppuccin/mdBook/releases/download/v0.1.1/mdbook-catppuccin-x86_64-apple-darwin.tar.gz"
-  sha256 "9e6cd573de0afdb4838ab71a3d23db4cf29a3d51468cba60579819ed11c15c11"
+  sha256 "7a0a2d543fbaa815bdc522491af75b182d0c1be2855c1562a35baa1f9fe23713"
   license "MIT"
 
   def install
