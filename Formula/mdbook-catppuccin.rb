bd5d33679dfbf3ed07874d0c105d15b025e6d68a
sgoudham <sgoudham@gmail.com>
2022-08-29 04:19:38 +0100

    build(ci): Try to fix builds on macos

--- Formula/mdbook-catppuccin.rb
+++ Formula/mdbook-catppuccin.rb
@@ -1,9 +1,16 @@
 class MdbookCatppuccin < Formula
   desc "🎊 Soothing pastel theme for mdBook"
   homepage "https://github.com/catppuccin/mdBook"
-  url "https://github.com/catppuccin/mdBook/releases/download/v0.1.1/mdbook-catppuccin-x86_64-unknown-linux-gnu.tar.gz"
   sha256 "9e6cd573de0afdb4838ab71a3d23db4cf29a3d51468cba60579819ed11c15c11"
   license "MIT"
+
+  on_macos do
+    url "https://github.com/catppuccin/mdBook/releases/download/v0.1.1/mdbook-catppuccin-x86_64-apple-darwin.tar.gz"
+  end
+
+  on_linux do
+    url "https://github.com/catppuccin/mdBook/releases/download/v0.1.1/mdbook-catppuccin-x86_64-unknown-linux-gnu.tar.gz"
+  end
 
   def install
     bin.install "mdbook-catppuccin"
