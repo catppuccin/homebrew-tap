2e89ada9be10cc83c8eb12ca4dd2a874348a99b2
winston <79978224+nekowinston@users.noreply.github.com>
2023-11-04 19:12:12 +0000

    ctp: add 2.0.0 bottle.

--- Formula/ctp.rb
+++ Formula/ctp.rb
@@ -6,6 +6,12 @@ class Ctp < Formula
   license "MIT"
   head "https://github.com/catppuccin/cli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/catppuccin/tap"
+    sha256 cellar: :any_skip_relocation, ventura:      "e55e62b2da4275d8e06c3cbdafc41b3a07bbcc67e0b164d07b8ac984861c19c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "04cc61a92e5a948ef058193b491d965ee7e719f4c04bf54c96216a23744eb789"
+  end
+
   depends_on "go" => :build
 
   def install
