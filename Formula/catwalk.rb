90958d4f9900c0a5a769018387ceaeb11c30618d
backwardspy <backwardspy@pigeon.life>
2024-06-02 12:16:22 +0000

    catwalk 1.3.1
    Closes #27.
    
    Signed-off-by: backwardspy <backwardspy@pigeon.life>

--- Formula/catwalk.rb
+++ Formula/catwalk.rb
@@ -1,8 +1,8 @@
 class Catwalk < Formula
   desc "Soothing preview image generator for the high-spirited!"
   homepage "https://github.com/catppuccin/toolbox/tree/main/catwalk"
-  url "https://github.com/catppuccin/toolbox/archive/refs/tags/catwalk-v1.3.0.tar.gz"
-  sha256 "e660ff504641d0eec9a69c2945970cfd121de9c7e6dd769c6ba3a72fed76700a"
+  url "https://github.com/catppuccin/toolbox/archive/refs/tags/catwalk-v1.3.1.tar.gz"
+  sha256 "b9093e5f93aa94526675c03f921f8d94d1832640d281b0c3f96fa00c00015213"
   license "MIT"
   head "https://github.com/catppuccin/toolbox.git", branch: "main"
 
